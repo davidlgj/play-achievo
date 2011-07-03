821fe432925c9aa218907bdd19626ca1fc5ef7e3
David Rosell <david@sure.(none)>
2011-07-03 11:47:46 +0200

    Change model to use JodaTime (LocalDate, Period)

--- app/models.scala
+++ app/models.scala
@@ -1,25 +1,22 @@
 package models
 
-import java.util.Date
-import java.util.Calendar
+import org.joda.time.{Period, DateTime, LocalDate}
 
+case class Activity(project: String, phase: String, time: Period, comment: String)
 
-case class Activity(project:String, phase:String, time:Float, comment:String)
-
-case class Day(activities:List[Activity], date:Date)
+case class Day(activities: List[Activity], date: LocalDate)
 
 object DataMocker {
 
-   def getTime(n:Int):Date =  {
-      val cal = Calendar.getInstance()
-      cal.add(Calendar.DAY_OF_MONTH,-n)
-      cal.getTime
+   def getTime(n: Int): LocalDate =  {
+       val dt = new DateTime
+       dt.plusDays(n).toLocalDate
    }
    
    def days(nr:Int):List[Day] = nr match {
       case -1 => Nil
       case n =>  Day(
-                       List( Activity("JS Absence","Public Holiday",8f,"foobar") ),
+                       List( Activity("JS Absence", "Public Holiday", Period.hours(7).plusMinutes(30), "foobar") ),
                        DataMocker.getTime(n)
                     ) :: days(n-1)
    }
