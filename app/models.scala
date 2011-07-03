d2f89156583bd86e9a4f1b1886dcebd1962c982e
David Rosell <david@sure.(none)>
2011-07-03 12:27:05 +0200

    Introducing Project and Phase to the model

--- app/models.scala
+++ app/models.scala
@@ -2,23 +2,31 @@ package models
 
 import org.joda.time.{Period, DateTime, LocalDate}
 
-case class Activity(project: String, phase: String, time: Period, comment: String)
+case class Project(id: String, title: String, phases: List[Phase])
 
-case class Day(activities: List[Activity], date: LocalDate)
+case class Phase(id: String, title: String)
+
+case class Activity(project: Project, phase: Phase, time: Period, comment: String)
+
+case class Day(activities: List[Activity], date: LocalDate) {
+    def this(activity: Activity, date: LocalDate) = this (List(activity), date)
+}
 
 object DataMocker {
+    val phase = Phase("apa", "Public Holiday")
+    val project = Project("bepa", "JS Absence", List(phase))
 
-   def getTime(n: Int): LocalDate =  {
-       val dt = new DateTime
-       dt.plusDays(n).toLocalDate
-   }
-   
-   def days(nr:Int):List[Day] = nr match {
-      case -1 => Nil
-      case n =>  Day(
-                       List( Activity("JS Absence", "Public Holiday", Period.hours(7).plusMinutes(30), "foobar") ),
-                       DataMocker.getTime(n)
-                    ) :: days(n-1)
-   }
+    def getTime(n: Int): LocalDate = {
+        val dt = new DateTime
+        dt.plusDays(n).toLocalDate
+    }
+
+    def days(nr: Int): List[Day] = nr match {
+        case -1 => Nil
+        case n => Day(
+            List(Activity(project, phase, Period.hours(7).plusMinutes(30), "foobar")),
+            DataMocker.getTime(n)
+        ) :: days(n - 1)
+    }
 
 }
